b2d2151f5c534dc339190670838c9031658dfcd3
Jack Yu <jack.yu@suse.com>
2024-12-30 16:21:41 +0800

    test
    Signed-off-by: Jack Yu <jack.yu@suse.com>

--- .github/workflows/bot.py
+++ .github/workflows/bot.py
@@ -20,7 +20,7 @@ SUPPORTED_EVENT = [
 if __name__ == "__main__":
     settings()
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.ERROR,
         format="%(asctime)s [%(levelname)s] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S", 
     )
